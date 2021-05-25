979a0b605cdcfed8f5d024a9ad943a68f51f9c3e
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-25 15:26:51 +0500

    new

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.get("/", (req, res) => {
 auth(app);
 notes(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
