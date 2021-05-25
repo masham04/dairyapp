2cefbe6a9e2ed5b459b5e90bb8af3d5337f1e5b9
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-25 15:14:54 +0500

    make it better

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ connectDB();
 
 const app = express();
 const PORT = 8000;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +22,7 @@ app.get("/", (req, res) => {
 auth(app);
 notes(app);
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
